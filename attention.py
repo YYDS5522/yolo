479db79672e1f20c7320b38bbf63e73144213def
YYBD <2711919824@qq.com>
2025-12-08 20:11:20 +0800

    Implement CPCA and CAFM classes for attention
    Added CPCA and CAFM classes for channel and spatial attention mechanisms.

--- attention.py
+++ attention.py
@@ -142,6 +142,85 @@ class AFCA(nn.Module):
         return input*out
 
 
+class CPCA(nn.Module):
+    def __init__(self, channels, channelAttention_reduce=4):
+        super().__init__()
+
+        self.ca = CPCA_ChannelAttention(input_channels=channels, internal_neurons=channels // channelAttention_reduce)
+        self.dconv5_5 = nn.Conv2d(channels,channels,kernel_size=5,padding=2,groups=channels)
+        self.dconv1_7 = nn.Conv2d(channels,channels,kernel_size=(1,7),padding=(0,3),groups=channels)
+        self.dconv7_1 = nn.Conv2d(channels,channels,kernel_size=(7,1),padding=(3,0),groups=channels)
+        self.dconv1_11 = nn.Conv2d(channels,channels,kernel_size=(1,11),padding=(0,5),groups=channels)
+        self.dconv11_1 = nn.Conv2d(channels,channels,kernel_size=(11,1),padding=(5,0),groups=channels)
+        self.dconv1_21 = nn.Conv2d(channels,channels,kernel_size=(1,21),padding=(0,10),groups=channels)
+        self.dconv21_1 = nn.Conv2d(channels,channels,kernel_size=(21,1),padding=(10,0),groups=channels)
+        self.conv = nn.Conv2d(channels,channels,kernel_size=(1,1),padding=0)
+        self.act = nn.GELU()
+
+    def forward(self, inputs):
+        #   Global Perceptron
+        inputs = self.conv(inputs)
+        inputs = self.act(inputs)
+        
+        inputs = self.ca(inputs)
+
+        x_init = self.dconv5_5(inputs)
+        x_1 = self.dconv1_7(x_init)
+        x_1 = self.dconv7_1(x_1)
+        x_2 = self.dconv1_11(x_init)
+        x_2 = self.dconv11_1(x_2)
+        x_3 = self.dconv1_21(x_init)
+        x_3 = self.dconv21_1(x_3)
+        x = x_1 + x_2 + x_3 + x_init
+        spatial_att = self.conv(x)
+        out = spatial_att * inputs
+        out = self.conv(out)
+        return out
+
+class CAFM(nn.Module):
+    def __init__(self, dim, num_heads=8, bias=False):
+        super(CAFM, self).__init__()
+        self.num_heads = num_heads
+        self.temperature = nn.Parameter(torch.ones(num_heads, 1, 1))
+
+        self.qkv = nn.Conv3d(dim, dim*3, kernel_size=(1,1,1), bias=bias)
+        self.qkv_dwconv = nn.Conv3d(dim*3, dim*3, kernel_size=(3,3,3), stride=1, padding=1, groups=dim*3, bias=bias)
+        self.project_out = nn.Conv3d(dim, dim, kernel_size=(1,1,1), bias=bias)
+        self.fc = nn.Conv3d(3*self.num_heads, 9, kernel_size=(1,1,1), bias=True)
+
+        self.dep_conv = nn.Conv3d(9*dim//self.num_heads, dim, kernel_size=(3,3,3), bias=True, groups=dim//self.num_heads, padding=1)
+
+    def forward(self, x):
+        b,c,h,w = x.shape
+        x = x.unsqueeze(2)
+        qkv = self.qkv_dwconv(self.qkv(x))
+        qkv = qkv.squeeze(2)
+        f_conv = qkv.permute(0,2,3,1) 
+        f_all = qkv.reshape(f_conv.shape[0], h*w, 3*self.num_heads, -1).permute(0, 2, 1, 3) 
+        f_all = self.fc(f_all.unsqueeze(2))
+        f_all = f_all.squeeze(2)
+        #local conv
+        f_conv = f_all.permute(0, 3, 1, 2).reshape(x.shape[0], 9*x.shape[1]//self.num_heads, h, w)
+        f_conv = f_conv.unsqueeze(2)
+        out_conv = self.dep_conv(f_conv) # B, C, H, W
+        out_conv = out_conv.squeeze(2)
+        # global SA
+        q,k,v = qkv.chunk(3, dim=1)   
+        q = rearrange(q, 'b (head c) h w -> b head c (h w)', head=self.num_heads)
+        k = rearrange(k, 'b (head c) h w -> b head c (h w)', head=self.num_heads)
+        v = rearrange(v, 'b (head c) h w -> b head c (h w)', head=self.num_heads)
+        q = torch.nn.functional.normalize(q, dim=-1)
+        k = torch.nn.functional.normalize(k, dim=-1)
+        attn = (q @ k.transpose(-2, -1)) * self.temperature
+        attn = attn.softmax(dim=-1)
+        out = (attn @ v)
+        out = rearrange(out, 'b head c (h w) -> b (head c) h w', head=self.num_heads, h=h, w=w)
+        out = out.unsqueeze(2)
+        out = self.project_out(out)
+        out = out.squeeze(2)
+        output = out + out_conv
+
+        return output
 
     
 
